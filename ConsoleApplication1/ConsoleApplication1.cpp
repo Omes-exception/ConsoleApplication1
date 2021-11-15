77376279935b69406641809557c64c0db35fc104
neogm <neogm@DESKTOP-E8PFMNS>
2021-11-15 16:44:39 +0100

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int x;
+	cout << "Say something funny";
+    cin >> x;
+	if (x == 69) {
+		cout << "Nice";
+	}
+	else { cout << "You should have entered 69 not funny"; }
+
+
+
+}
